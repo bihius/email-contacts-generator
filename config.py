0505269634e14a1ab4da2916f4801172154d308a
Bihius <k.kasperkiewicz@proton.me>
2023-11-25 11:09:14 +0100

    fix variable names

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-EMAILS_USER = "" # type your email address
+EMAIL_USER = "" # type your email address
 EMAIL_PASS = "" # type your password
 SERVER = "" # type your email server eg. imap.gmail.com
 PORT = 993 # this is default port for secure connections   
